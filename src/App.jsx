9d197a184d11772aef4608de7993a122953c9f6a
Muhammad Ahmed <153613965+Muhammadahmedanis@users.noreply.github.com>
2025-03-01 09:34:44 +0500

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Sidebar from './components/Sidebar'
 import DashboardPage from './routes/Dashaboard'
-import { Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Layout from './Layout.jsx/Layout'
 import Calendar from './routes/calender'
 import Analytics from './routes/Analytics'
-import Login from './routes/login'
+import Login from './routes/Login'
 import Signup from './routes/signup'
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {
   Building2, Plus, X, Briefcase 
 } from 'lucide-react';
 import { useNavigate } from 'react-router-dom'; 
-import { v4 as uuidv4 } from 'uuid'; // Generate unique workspace ID
+import { v4 as uuidv4 } from 'uuid';
 
 const Navbar = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
